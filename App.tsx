ab0bda872bf704b05be573f9c80ca1c16ffc4103
AyparaKerimova <eelify1@gmail.com>
2024-04-24 03:44:41 +0400

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
+
+import React,{useEffect} from 'react';
 import {Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View>
       <Text>Hello</Text>
