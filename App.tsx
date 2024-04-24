4ab237c5f3b4e3c317147cc9bd79ad04722f4e9a
AyparaKerimova <eelify1@gmail.com>
2024-04-24 17:30:19 +0400

    typography

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@
 
 import React,{useEffect} from 'react';
 import {Text, View} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
 
-function App(): React.JSX.Element {
+export default function App(): React.JSX.Element {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <View>
+    <SafeAreaView>
+    <View >
       <Text>Hello</Text>
     </View>
+    </SafeAreaView>
   );
 }
-export default App;
+
