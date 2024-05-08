db397ebc1313125a9e75d6529617f531761fca1f
abbasoffanermin <abbasoffanermin2004@gmail.com>
2024-05-08 21:05:58 +0400

    welcome screen update

--- src/screens/auth/WelcomeScreen.tsx
+++ src/screens/auth/WelcomeScreen.tsx
@@ -19,6 +19,10 @@ import {TypographyStyles} from '../../theme/typography';
 import {colors} from '../../theme/colors';
 import {onboarding} from '../../constants/onboarding';
 import {Pagination} from '../../components/Pagination';
+import {normalize} from '../../theme/metrics';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {NavigationParamlist} from '../../types/navigatorTypes';
+import {Routers} from '../../router/routers';
 
 type OnboardingItem = {
   id: number;
@@ -26,7 +30,15 @@ type OnboardingItem = {
   title: string;
 };
 
-export const WelcomeScreen = () => {
+export const WelcomeScreen: React.FC<
+  NativeStackScreenProps<NavigationParamlist, Routers.welcome>
+> = ({navigation}) => {
+  const navigateToLogin = () => {
+    navigation.navigate(Routers.login);
+  };
+  const navigateToRegister = () => {
+    navigation.navigate(Routers.register);
+  };
   const renderItem = ({item}: {item: OnboardingItem}) => {
     return (
       <View style={styles.background}>
@@ -35,10 +47,30 @@ export const WelcomeScreen = () => {
         <Image
           source={item.image}
           resizeMode={item.id === 0 ? 'cover' : 'contain'}
-          style={styles.image}
+          style={item.id !== 0 ? styles.image2 : styles.image}
         />
-        <Text style={TypographyStyles.title1}>{item.title}</Text>
+        <Text
+          style={[TypographyStyles.title2, item.id === 0 ? {} : styles.pad]}>
+          {item.title}
+        </Text>
         <Pagination selectedIndex={item.id} />
+        <View style={{gap: 20, marginTop: 20}}>
+          <ButtonPrimary label="Create an account" primaryBlock centered onPress={navigateToRegister} />
+          <ButtonPrimary
+            label="Log in Instead"
+            onPress={navigateToLogin}
+            primaryBlock
+            centered
+            customStyles={styles.button}
+          />
+        </View>
+        {item.id !== 0 ? (
+          <View style={styles.termsView}>
+            <Text style={TypographyStyles.SmallNormalRegular}>Help</Text>
+            <View style={styles.divider} />
+            <Text style={TypographyStyles.SmallNormalRegular}>Terms</Text>
+          </View>
+        ) : null}
       </View>
     );
   };
@@ -59,636 +91,14 @@ export const WelcomeScreen = () => {
   );
 };
 
-<ScrollView>
-  <ButtonPrimary
-    label="Block Primary"
-    showArrowSign={false}
-    showMessageSign={false}
-    primaryBlock
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    primaryBlock
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={true}
-    showMessageSign={false}
-    primaryBlock
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    centered
-    primaryBlock
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={false}
-    disabled
-    primaryBlock
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    primaryBlock
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={true}
-    showMessageSign={false}
-    primaryBlock
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    primaryBlock
-    centered
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    hasArrowSign={false}
-    hasMessageSign={false}
-    primaryLarge
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    primaryLarge
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasMessageSign={false}
-    primaryLarge
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    centered
-    primaryLarge
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    hasArrowSign={false}
-    hasMessageSign={false}
-    primaryLarge
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    primaryLarge
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasMessageSign={false}
-    primaryLarge
-    disabled
-  />
-  <ButtonPrimary
-    label="Button Large"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    centered
-    primaryLarge
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    hasArrowSign={true}
-    hasMessageSign={true}
-    primarySmall
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    hasArrowSign={false}
-    hasMessageSign={true}
-    showMessageSign={true}
-    primarySmall
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={true}
-    showMessageSign={false}
-    showArrowSign={true}
-    showMessageSign={false}
-    hasArrowSign={true}
-    hasMessageSign={false}
-    primarySmall
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    hasArrowSign={false}
-    showMessageSign={true}
-    hasMessageSign={true}
-    centered
-    primarySmall
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    showMessageSign={false}
-    hasArrowSign={false}
-    hasMessageSign={false}
-    centered
-    primarySmall
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    hasArrowSign={false}
-    hasMessageSign={true}
-    showMessageSign={true}
-    primarySmall
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasArrowSign={true}
-    hasMessageSign={false}
-    primarySmall
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Primary"
-    showArrowSign={false}
-    hasArrowSign={false}
-    showMessageSign={true}
-    hasMessageSign={true}
-    primarySmall
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={false}
-    secondaryBlock
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    secondaryBlock
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    secondaryBlock
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    centered
-    secondaryBlock
-  />
-  <ButtonPrimary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={false}
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    disabled
-  />
-  <ButtonPrimary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    centered
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={false}
-    secondaryLarge
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    secondaryLarge
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasMessageSign={false}
-    secondaryLarge
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    centered
-    secondaryLarge
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={false}
-    secondaryLarge
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    secondaryLarge
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasMessageSign={false}
-    secondaryLarge
-    disabled
-  />
-  <ButtonSecondary
-    label="Button Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    secondaryLarge
-    centered
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    hasArrowSign={true}
-    hasMessageSign={true}
-    secondarySmall
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={true}
-    showMessageSign={true}
-    secondarySmall
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasArrowSign={true}
-    hasMessageSign={false}
-    secondarySmall
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    hasArrowSign={false}
-    showMessageSign={true}
-    hasMessageSign={true}
-    centered
-    secondarySmall
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={false}
-    hasArrowSign={false}
-    hasMessageSign={false}
-    centered
-    secondarySmall
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={true}
-    showMessageSign={true}
-    secondarySmall
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasArrowSign={true}
-    hasMessageSign={false}
-    secondarySmall
-    disabled
-  />
-  <ButtonSecondary
-    label="Block+Secondary"
-    showArrowSign={false}
-    hasArrowSign={false}
-    showMessageSign={true}
-    hasMessageSign={true}
-    secondarySmall
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    outlineBlock
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    outlineBlock
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={true}
-    showMessageSign={false}
-    outlineBlock
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    centered
-    outlineBlock
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    outlineBlock
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    outlineBlock
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={true}
-    showMessageSign={false}
-    outlineBlock
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    outlineBlock
-    centered
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={false}
-    outlineLarge
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    outlineLarge
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasMessageSign={false}
-    outlineLarge
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    centered
-    outlineLarge
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={false}
-    outlineLarge
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    outlineLarge
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasMessageSign={false}
-    outlineLarge
-    disabled
-  />
-  <ButtonOutline
-    label="Button Secondary"
-    showArrowSign={false}
-    showMessageSign={true}
-    hasArrowSign={false}
-    centered
-    outlineLarge
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    hasArrowSign={true}
-    hasMessageSign={true}
-    outlineSmall
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={true}
-    showMessageSign={true}
-    outlineSmall
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasArrowSign={true}
-    hasMessageSign={false}
-    outlineSmall
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={false}
-    hasArrowSign={false}
-    showMessageSign={true}
-    hasMessageSign={true}
-    centered
-    outlineSmall
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={false}
-    showMessageSign={false}
-    hasArrowSign={false}
-    hasMessageSign={false}
-    centered
-    outlineSmall
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    hasArrowSign={false}
-    hasMessageSign={true}
-    showMessageSign={true}
-    outlineSmall
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={true}
-    showMessageSign={false}
-    hasArrowSign={true}
-    hasMessageSign={false}
-    outlineSmall
-    disabled
-  />
-  <ButtonOutline
-    label="Block+Secondary"
-    showArrowSign={false}
-    hasArrowSign={false}
-    showMessageSign={true}
-    hasMessageSign={true}
-    outlineSmall
-    disabled
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    transparentBlock
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    transparentBlock
-    centered
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    transparentBlock
-    disabled
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    transparentBlock
-    centered
-    disabled
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    transparentLarge
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    transparentLarge
-    centered
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    transparentLarge
-    disabled
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    transparentLarge
-    centered
-    disabled
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    transparentSmall
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    transparentSmall
-    centered
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={false}
-    transparentSmall
-    disabled
-  />
-  <ButtonTransparent
-    label="Block+Outline"
-    showArrowSign={false}
-    showMessageSign={true}
-    transparentSmall
-    centered
-    disabled
-  />
-  <Text> Welcome Screen </Text>
-</ScrollView>;
-
 const styles = StyleSheet.create({
   root: {
     flex: 1,
     backgroundColor: colors.sky.lightest,
+  },
+  button: {
+    backgroundColor: colors.ink.base,
+    alignItems: 'center',
   },
   contentContainerStyle: {},
   background: {
@@ -697,12 +107,22 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     paddingBottom: 16,
     paddingHorizontal: 16,
-    gap: 24,
+    gap: 5,
+  },
+  image2: {
+    marginBottom: 100,
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    flex: 1,
+    right: 0,
+    bottom: 0,
   },
   image: {
     width: '100%',
     height: '100%',
     position: 'absolute',
+
     flex: 1,
     right: 0,
     bottom: 0,
@@ -723,7 +143,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     position: 'absolute',
     zIndex: 1,
-    top: 76,
+    top: 56,
     left: 154,
+  },
+  pad: {
+    marginLeft: 16,
+    width: 327,
+    height: 72,
+    textAlign: 'center',
+  },
+  termsView: {
+    gap: normalize('horizontal', 8),
+    paddingTop: normalize('vertical', 54),
+    ...CommonStyles.alignJustifyCenterRow,
+  },
+  divider: {
+    width: 1,
+    height: 15,
+    backgroundColor: colors.ink.lighter,
   },
 });
