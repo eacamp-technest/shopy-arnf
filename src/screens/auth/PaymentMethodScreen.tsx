9bbb34e91812f374e881a6ee0ce199408510bf61
abbasoffanermin <abbasoffanermin2004@gmail.com>
2024-05-11 20:31:45 +0400

    tablesComponent

--- src/screens/auth/PaymentMethodScreen.tsx
+++ src/screens/auth/PaymentMethodScreen.tsx
@@ -1,10 +1,20 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Switch} from 'react-native';
+import React, {useState} from 'react';
+import {Tables} from '../../components/Tables';
+import Facebook from '../../../assets/vectors/facebook.svg';
+import {colors} from '../../theme/colors';
 
 export const PaymentMethodScreen = () => {
   return (
     <View>
-      <Text>PaymentMethodScreen</Text>
+      <Tables
+        content="Content is here"
+        rightType={'button'}
+        right="small"
+        caption="for caption"
+        leftType="icon"
+        left={<Facebook width={24} height={24} color={colors.primary.base} />}
+      />
     </View>
   );
 };
