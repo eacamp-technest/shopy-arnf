8d78aabe1b8afa946118605e56b6b1b22b371f52
abbasoffanermin <abbasoffanermin2004@gmail.com>
2024-10-05 14:31:23 +0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -30,7 +30,7 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
   },
 });
 export default gestureHandlerRootHOC(App);
